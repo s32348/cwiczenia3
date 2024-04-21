9c6b43142abe8a64a80f8bcef2733c20ed2f201f
Jacek Kłobukowski <s32348@pjwstk.edu.pl>
2024-04-21 18:55:02 +0200

    zrobione na cwiczeniach

--- cwiczenia3/src/Employee.java
+++ cwiczenia3/src/Employee.java
@@ -1,59 +1,74 @@
 public class Employee {
-    String firstname;
-    String lastname;
+    String firstName;
+    String lastName;
     String city;
     String email;
     int pesel;
-    int joindate;
+    int joinDate;
     int salary;
-    public Employee(String firstname, String lastname, String city, String email, int pesel, int joindate) {
-        this.firstname = firstname;
-        this.lastname = lastname;
+
+    public Employee(String firstName, String lastName, String city, String email, int pesel, int joinDate, int salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.city = city;
         this.email = email;
         this.pesel = pesel;
-        this.joindate = joindate;
-        this.salary= salary;
+        this.joinDate = joinDate;
+        this.salary = salary;
     }
-    public String getFirstname() {
-        return firstname;
+
+    public String getFirstName() {
+        return firstName;
     }
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
-    public String getLastname() {
-        return lastname;
+
+    public String getLastName() {
+        return lastName;
     }
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
+
     public String getCity() {
         return city;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public int getPesel() {
         return pesel;
     }
+
     public void setPesel(int pesel) {
         this.pesel = pesel;
     }
-    public int getJoindate() {
-        return joindate;
+
+    public int getJoinDate() {
+        return joinDate;
     }
-    public void setJoindate(int joindate) {
-        this.joindate = joindate;
+
+    public void setJoinDate(int joinDate) {
+        this.joinDate = joinDate;
     }
+
     public int getSalary() {
         return salary;
     }
+
     public void setSalary(int salary) {
         this.salary = salary;
     }
